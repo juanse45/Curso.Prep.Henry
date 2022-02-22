04c6b1897253a40cd16916d6ea9caf0d5a6ce570
juan Bonilla <juanse45@yahoo.com>
2022-02-21 22:39:10 -0500

    homework hasta punto 7 v1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,13 +10,13 @@ const nuevoNum = 45;
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  return str;
   
 }
 
@@ -35,6 +36,10 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  function suma(x,y) {
+var suma = x + y;
+return suma;
+  }
   
 }
 
