e07b24e72316ad62227fa6bfc742b2fe9021a5cb
juan Bonilla <juanse45@yahoo.com>
2022-02-24 21:24:08 -0500

    Homework 2 hasta punto 1 v1

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,15 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if(x>y){
+    return x
+   }
+  else if (y>x) {
+    return y
+  }
+  else if (x===y){
+    return x
+  }
 }
 
 function mayoriaDeEdad(edad) {
